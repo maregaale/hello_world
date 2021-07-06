aef0634f53a44d27594dd6eaa590f929b4c6aed4
Alessandro Marega <ale_marega@hotmail.it>
2021-07-06 11:49:12 +0200

    hello world

---
+++ hello_world/hello_world.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	//print
+	cout << "Hello World! \n";
+	cout << "I am learning C++ \n \n";
+
+	//number stamp
+	int myNum = 15;
+	cout << myNum << endl;
+
+	return 0;
+}
+
